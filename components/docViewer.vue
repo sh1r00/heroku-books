4b7a10d7becbad3de383bcf398bfb6cfa2555a72
sh1r00 <rasisg@gmail.com>
2020-09-20 07:46:36 -0700

    .

--- components/docViewer.vue
+++ components/docViewer.vue
@@ -2,11 +2,13 @@
   <div>
     <iframe
       class="doc-viewer"
-      :src="`https://docs.google.com/gview?url=${baseUrl}${fileUrl}%26embedded=true%26delayms=3000`"
+      :src="`https://docs.google.com/gview?url=${baseUrl}${fileUrl}&embedded=true&delayms=3000`"
       frameborder="0"
     ></iframe>
     <iframe
-      :src="`https://docs.google.com/viewer?url=${baseUrl}${fileUrl}%26embedded=true%26delaysms=5000`"
+      class="doc-viewer"
+      :src="`https://docs.google.com/viewer?url=${baseUrl}${fileUrl}&embedded=true&delaysms=3000`"
+      frameborder="0"
     />
   </div>
 </template>
